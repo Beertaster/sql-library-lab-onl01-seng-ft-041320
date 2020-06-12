8e20110d7d36239b34126e9ec5d8b4cdf3a753fd
William Stevens <wstevens.hornet@gmail.com>
2020-06-12 00:28:21 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -18,7 +18,7 @@ INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Sam
 INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Corpral Carrot", "Heir of the city", "City Gaurd", 2, 2);
 INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Constable Knobby Knobs", "disqualified from the human race for loitering", "City Gaurd", 2, 2);
 INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("The Patrician", "A city that works", "Politician", 2, 2);
-INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Frodo Baggins", "The Bravest Hobbit", "Hobbit", 1, 1);
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Geralt of Rivai", "Moster hunter", "Witcher", 1, 1);
 INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Saruman", "The White Wizard", "Wizard", 1, 1);
 INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Lurtz", "An Army Worthy of Mordor", "Uruk-hai", 1, 1);
 INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Shelob", "She likes to feed", "Giant Spider", 1, 1);
